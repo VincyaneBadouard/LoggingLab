574294b8da5b639dcc0f7f7565025365a48ad115
Vincyane Badouard <you@example.com>
2021-02-12 17:49:19 +0100

    small additions

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Data title
+#'
+#' Data description
+#'
+#' @format A data frame with X rows and Z variables:
+#' \describe{
+#'   \item{var1}{description, in units}
+#'   \item{var2}{description, in units}
+#'   ...
+#' }
+#' @source \url{}
