c69e0d5da9136d607b27dd63284c2fdd0e4d6e04
Vincyane Badouard <you@example.com>
2021-04-08 13:58:53 +0100

    Collaboration exercise and presentation

---
+++ R/bonjour.R
@@ -0,0 +1,19 @@
+#' Say "Bonjour !" with a name
+#'
+#' @param nom (character) Name of the person to say bonjour.
+#' @param verbose (boolean) Print in console
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' bonjour()
+bonjour <- function(
+  nom = "Maria",
+  verbose = T
+){
+  sentence <- paste("Bonjour!", nom)
+  if(verbose)
+    cat(sentence)
+  return(sentence)
+}
