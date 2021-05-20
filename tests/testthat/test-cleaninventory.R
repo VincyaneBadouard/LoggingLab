40b8a43733cd5ebbe523123ae78f7003d9993d59
Vincyane Badouard <you@example.com>
2021-05-20 09:30:35 +0100

    Better functions and tests

--- tests/testthat/test-cleaninventory.R
+++ tests/testthat/test-cleaninventory.R
@@ -1,11 +1,54 @@
 test_that("cleaninventory", {
+
+  #load the test inventory
   load(system.file("extdata", "BrokenParacou6_2016.rda", package = "Maria"))
-  # expect_true(cleaninventory(data), InventoryClean$CodeAlive == "TRUE") #ca marche pas
-  # expect_true(data$CodeAlive, InventoryClean$CodeAlive == "TRUE") #ca marche pas*
-  expect_type(BrokenParacou6_2016$idTree, 'character')
-  expect_error(cleaninventory(inventory = BrokenParacou6_2016))
+
+  #check if CodeAlive == TRUE
+  expect_true(all(cleaninventory(Paracou6_2016)$CodeAlive == "TRUE"))
+
+  #check if DBH >= 10 (check if DBH exists in the same way)
+  expect_true(all(cleaninventory(Paracou6_2016)$DBH >= 10))
+
+  # check if idTree's are unique
+  expect_false(any(duplicated(cleaninventory(Paracou6_2016)$idTree)))
+
+  #check if it is always the same plot
+  expect_false(!length(unique(cleaninventory(Paracou6_2016)$Plot))== 1)
+
+  #and the same year
+  expect_false(!length(unique(cleaninventory(Paracou6_2016)$CensusYear))== 1)
+
+  #check if the stops work
+  expect_error(cleaninventory(BrokenParacou6_2016), regexp = "idTree")
+
 })
-#si ma fct fctne, dans inventory=BrockenParacou6_2016 :
-# CodeAlive == TRUE
-# CircCorr >= 10
-# et ils me renvoie des msg d'erreurs
+
+
+# test_that("cleaninventory", {
+#   load(system.file("extdata", "BrokenParacou6_2016.rda", package = "Maria"))
+#   BrokenParacou6_2016 <- filter(BrokenParacou6_2016, idTree != 200) # eliminate the first detected error
+#
+#   expect_error(cleaninventory(BrokenParacou6_2016), regexp = "Plot")
+# })
+# #
+# test_that("cleaninventory", {
+#   load(system.file("extdata", "BrokenParacou6_2016.rda", package = "Maria"))
+#   BrokenParacou6_2016 <- filter(BrokenParacou6_2016, idTree != "200") # eliminate the first detected error
+#   BrokenParacou6_2016 <- filter(BrokenParacou6_2016, PLOT != "100") # eliminate the first detected error
+#
+#
+#   expect_error(cleaninventory(BrokenParacou6_2016), regexp = "CensusYear")
+# })
+
+
+
+# only keep CodeAlive == TRUE -> check if CodeAlive == TRUE
+# create DBH column and only keep DBH >= 10 -> check if DBH >= 10 (check if DBH exists in the same way)
+# check if idTree's are unique
+# check if it is always the same plot
+# and the same year
+# stop when at least one condition is not checked -> check if the stops work
+
+
+
+#I use BrockenParacou6_2016 to check error message, and Paracou6_2016 to check other actions.
--- tests/testthat/test-inventorycheckformat.R
+++ tests/testthat/test-inventorycheckformat.R
@@ -1,5 +1,31 @@
-test_that("datacheckformat", {
+test_that("inventorycheckformat", {
+
+  #load the test inventory
   load(system.file("extdata", "BrokenParacou6_2016.rda", package = "Maria"))
-  expect_error(datacheckformat(data=BrockenParacou6_2016))
+
+   #check if all the variables are detected
+  expect_true(all(c("Plot","CensusYear","idTree","Family","Genus","Species","CircCorr","CodeAlive","CommercialSp",
+                    "UTMZone","Lat","Lon","VernName","Xfield","Yfield","Xutm","Yutm")%in% names(inventorycheckformat(Paracou6_2016))))
+
+  #check if class to detect are the right ones
+  expect_type(c(inventorycheckformat(Paracou6_2016)$idTree, inventorycheckformat(Paracou6_2016)$UTMZone,
+                inventorycheckformat(Paracou6_2016)$CensusYear), "integer") #idTree, UTMZone, CensusYear
+
+  expect_type(c(inventorycheckformat(Paracou6_2016)$Plot, inventorycheckformat(Paracou6_2016)$Family, inventorycheckformat(Paracou6_2016)$Genus,
+                inventorycheckformat(Paracou6_2016)$Species, inventorycheckformat(Paracou6_2016)$VernName), "character") #Plot, Family, Genus, Species, VernName
+
+  expect_type(c(inventorycheckformat(Paracou6_2016)$Xfield, inventorycheckformat(Paracou6_2016)$Yfield, inventorycheckformat(Paracou6_2016)$Xutm,
+                inventorycheckformat(Paracou6_2016)$Yutm, inventorycheckformat(Paracou6_2016)$Lat,
+                inventorycheckformat(Paracou6_2016)$Lon, inventorycheckformat(Paracou6_2016)$CircCorr), "double") #Xfield, Yfield, Xutm, Yutm, Lat, Lon, CircCorr | DBH
+
+  expect_type(c(inventorycheckformat(Paracou6_2016)$CommercialSp, inventorycheckformat(Paracou6_2016)$CodeAlive), "logical") #CommercialSp, CodeAlive
+
+  #check if the stops work
+  expect_error(datacheckformat(BrockenParacou6_2016))
 })
+
+#check if all the variables are here -> detected
+#and if they are of the right class -> check if class to detect are the right ones
+#stop when at least one condition is not checked -> check if the stops work
+
 
