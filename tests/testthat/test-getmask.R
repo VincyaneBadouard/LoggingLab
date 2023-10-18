e626507866554b20e43fecf99802a20db9aa0b13
Eric Marcon <eric.marcon@agroparistech.fr>
2023-10-18 14:46:22 +0200

    updated test according to issue #67

--- tests/testthat/test-getmask.R
+++ tests/testthat/test-getmask.R
@@ -2,5 +2,6 @@ test_that("getmask", {
   data(Paracou6_2016)
   data(DTMParacou)
   mask <- getmask(Paracou6_2016, DTMParacou)
-  expect_true(inherits(mask, "SpatialPolygonsDataFrame"))
+  # See issue #67 for possible error
+  expect_true(inherits(mask, "SpatialPolygons"))
 })
