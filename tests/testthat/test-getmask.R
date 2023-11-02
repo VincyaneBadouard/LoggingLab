fb93c4caa21b7be769286fdb8b93749f57beaa99
Your Name <you@example.com>
2023-11-02 11:30:32 +0100

    Re-change getmask test

--- tests/testthat/test-getmask.R
+++ tests/testthat/test-getmask.R
@@ -3,5 +3,5 @@ test_that("getmask", {
   data(DTMParacou)
   mask <- getmask(Paracou6_2016, DTMParacou)
   # See issue #67 for possible error
-  expect_true(inherits(mask, "SpatialPolygonsDataFrame"))
+  expect_true(inherits(mask, "SpatialPolygons"))
 })
